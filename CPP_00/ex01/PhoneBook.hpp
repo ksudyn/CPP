abf7d462e784b0b47e19f250a95a8d3c3bba4ec4
Konrad Sudyn <ksudyn@c1r8s6.42madrid.com>
2025-07-09 20:46:14 +0200

    inicio de cpp

--- CPP_00/ex01/PhoneBook.hpp
+++ CPP_00/ex01/PhoneBook.hpp
@@ -6,14 +6,17 @@
 /*   By: ksudyn <ksudyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 19:37:11 by ksudyn            #+#    #+#             */
-/*   Updated: 2025/07/09 19:38:35 by ksudyn           ###   ########.fr       */
+/*   Updated: 2025/07/09 20:10:20 by ksudyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_H
 # define PHONEBOOK_H
 
-# include <iostream>
+#include <iostream>   // cout, cin
+#include <string>     // std::string
+#include <iomanip>    // setw, right
+#include <limits>     // para limpiar el input buffer
 
 class Contact
 {
