a5634c9c88a90d72fcddbf7c6fa1930426546eb8
Konrad Sudyn <ksudyn@c1r8s6.42madrid.com>
2025-07-09 19:53:21 +0200

    inicio de cpp

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksudyn <ksudyn@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 18:51:36 by ksudyn            #+#    #+#             */
+/*   Updated: 2025/07/09 19:21:23 by ksudyn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "string.h"
+#include "iostream"
+
+int main(int argc, char **argv)
+{
+    int i;
+    int j;
+    int len;
+
+    i = 1;
+    if(argc > 1)
+    {
+        while(i < argc)
+        {
+            j = 0;
+            len = strlen(argv[i]);
+            while(j <= len)
+            {
+                toupper(argv[i][j]);
+                j++;
+            }
+            std::cout << argv[i];
+            i++;
+        }
+        std::cout << "\n";
+		return (0);
+    }
+    std::cout <<"* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return (0);
+}
+
+// std::cout significa Console Output (salida por consola)
+// y es una variable del tipo ostream que pertenece al namespace std (standard).
+// Lo usas para imprimir texto u otras cosas en la consola,
+// parecido a printf() en C, pero más seguro y flexible.
+//cout es un objeto(no una funcion) que representa la salida estándar.
---
+++ CPP_00/ex01/PhoneBook.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksudyn <ksudyn@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 19:37:11 by ksudyn            #+#    #+#             */
+/*   Updated: 2025/07/09 19:38:35 by ksudyn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+# include <iostream>
+
+class Contact
+{
+	private:
+
+    public:
+};
+
+class PhoneBook
+{
+	private:
+
+	public:
+
+};
+
+
+#endif
