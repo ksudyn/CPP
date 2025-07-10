fe5c1edcea8e893708c81f35a0ce0bf60b5297a7
Konrad Sudyn <ksudyn@c1r9s6.42madrid.com>
2025-07-10 17:26:47 +0200

    procesando el ex01

--- CPP_00/ex01/Contact.cpp
+++ CPP_00/ex01/Contact.cpp
@@ -6,8 +6,125 @@
 /*   By: ksudyn <ksudyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 19:31:44 by ksudyn            #+#    #+#             */
-/*   Updated: 2025/07/09 19:39:25 by ksudyn           ###   ########.fr       */
+/*   Updated: 2025/07/10 17:24:08 by ksudyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
+
+Contact::Contact(void)
+{
+    std::cout << "Contact Constructor" <<std::endl;
+}
+Contact::~Contact(void)
+{
+    std::cout << "Contact Destructor" <<std::endl;
+}
+
+void Contact:: print_table_contact(void)
+{
+	std::cout << "First_name: " << this->first_name <<std::endl;
+	std::cout << "Last_name: " << this->last_name <<std::endl;
+	std::cout << "Nickname: " << this->nick_name <<std::endl;
+	std::cout << "Phone_number: " << this->phone_number <<std::endl;
+	std::cout << "Dark_secret: " << this->dark_secret <<std::endl;
+}
+
+int	Contact:: check_num_phone(const char *number)
+{
+	int i;
+
+	if (strlen(number) != 9)
+	{
+		std::cout << "Wrong phone number" <<std::endl;
+		return(1);
+	}
+	i = 0;
+	while (number[i] != '\0')
+	{
+		if (!(number[i] >= '0' && number[i] <= '9'))
+		{
+			std::cout << "Wrong phone number" <<std::endl;
+			return (1);
+		}
+		i++;
+	}
+	this->phone_number = atol(number);
+	return (0);
+}
+
+void Contact:: print_col(std::string str,  int len_str)
+{
+	int spaces;
+	int j = 0;
+	int i = 0;
+
+	spaces = 10 - len_str;
+	while (i < spaces)
+	{
+		std::cout << " ";
+		i++;
+	}
+	while (j < 9 && j < len_str)
+	{
+		std::cout << str[j++];
+	}
+	if (len_str >= 10)
+		std::cout << ".";
+}
+
+void Contact:: print_contact(int i)
+{
+	std::stringstream index;
+
+	index << (i +  1);
+	print_col(index.str(), 1);
+	std::cout << "|";
+	print_col(this->first_name, first_name.length());
+	std::cout << "|";
+	print_col(this->last_name, last_name.length());
+	std::cout << "|";
+	print_col(this->nick_name, nick_name.length());
+	std::cout << "\n";
+}
+
+int	Contact:: dat_contact(int *num_contact)
+{
+	std::string	input[5];
+	std::string	types[5];
+
+	types[0] = "    First_name";
+	types[1] = "    Last_name";
+	types[2] = "    Nickname";
+	types[3] = "    Phone_number";
+	types[4] = "    Dark_secret";
+	for (size_t i = 0; i < 5; i++)
+	{
+		std::cout << types[i] << ": ";
+		std::getline(std::cin, input[i]);
+		if (std::cin.eof())
+		{
+			std::cout << "\nEOF" <<std::endl;
+			return (1);
+		}
+		if (input[i].empty())
+		{
+			std::cout << "Empty contact info" <<std::endl;
+			(*num_contact)--;
+			return (0);
+		}
+		if (i == 3)
+		{
+			if (check_num_phone(input[3].c_str()) == 1)
+			{
+				(*num_contact)--;
+				return (0);
+			}
+		}
+	}
+	this->first_name = input[0];
+	this->last_name = input[1];
+	this->nick_name = input[2];
+	this->dark_secret = input[4];
+	return (0);
+}
--- CPP_00/ex01/PhoneBook.hpp
+++ CPP_00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: ksudyn <ksudyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 19:37:11 by ksudyn            #+#    #+#             */
-/*   Updated: 2025/07/09 20:10:20 by ksudyn           ###   ########.fr       */
+/*   Updated: 2025/07/10 17:25:33 by ksudyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,36 @@
 # define PHONEBOOK_H
 
 #include <iostream>   // cout, cin
-#include <string>     // std::string
+#include <string.h>     // std::string
 #include <iomanip>    // setw, right
 #include <limits>     // para limpiar el input buffer
 
+//....Contact....//
+//Representa a una persona en tu agenda.
+//Guarda datos de una persona: nombre, apellido, apodo, teléfono, secreto.
+//Tiene funciones para establecer (guardar) esos datos y para mostrarlos.
 class Contact
 {
 	private:
-
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nick_name;
+		long	phone_number;
+		std::string	dark_secret;
     public:
+		Contact(void);
+		~Contact(void);
+		void Contact:: print_table_contact(void);
+		int	Contact:: check_num_phone(const char *number);
+		void Contact:: print_col(std::string str,  int len_str);
+		void Contact:: print_contact(int i);
+		int	Contact:: dat_contact(int *num_contact);
 };
+//Los atributos son private porque no deben ser modificados directamente desde fuera.
+//Las funciones públicas son la "interfaz" para interactuar con esos datos.
 
+
+//....PhoneBook....//
 class PhoneBook
 {
 	private:
@@ -35,3 +54,12 @@ class PhoneBook
 
 
 #endif
+
+//Una clase es como un molde para crear objetos que contienen datos
+//(atributos) y funciones (métodos) que trabajan con esos datos.
+// class NombreClase {
+// private:
+//     // variables que solo la clase puede usar internamente
+// public:
+//     // funciones que otros pueden usar
+// };
